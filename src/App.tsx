41081e121d57e809f0c171015b63df0826f8ad66
Kyusung Sohn <rok.ksohn@gmail.com>
2025-11-29 19:45:59 +0900

    Chore: setup tailwindcss & related plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const name = "james";
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="min-h-screen">
+      <p className="custom-class custom-class-1 bg-red-500 text-center text-4xl font-bold text-yellow-500 hover:text-blue-500">
+        {name}
       </p>
-    </>
-  )
+      <p className="custom-class custom-class-3 text-4xl hover:bg-red-500">
+        Hello world!
+      </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
