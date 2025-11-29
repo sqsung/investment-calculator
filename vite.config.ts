4ed14475ed5d21b39539736876ff27b10bfc80bd
Kyusung Sohn <rok.ksohn@gmail.com>
2025-11-29 21:18:17 +0900

    Chore: setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -12,4 +13,9 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
